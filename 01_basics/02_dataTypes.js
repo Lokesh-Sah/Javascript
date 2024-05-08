1dfbbac24e25bdcec6800a1374fed0386e42841d
Lokesh-Sah <120782223+Lokesh-Sah@users.noreply.github.com>
2024-05-08 08:18:54 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser not try to use
+
+let age = 23
+let isLoggedIn = true
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string = ""
+// boolean = true / false
+// null = standalone value
+// undefined
+console.log(typeof null);
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // o/p undefined
+console.log(typeof null); // o/p object
