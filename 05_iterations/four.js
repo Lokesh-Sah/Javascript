337943cbc238f1af93fcd77aee9ea6aa13df7a91
Lokesh-Sah <120782223+Lokesh-Sah@users.noreply.github.com>
2024-05-15 15:54:11 +0000

    four-iteration

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -6,5 +6,21 @@ const myObject = {
 }
 
 for (const key in myObject) {
-    console.log(`${key} shortcut is for ${myObject[key]}`);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+
+const programming = ["js", "java", "cpp", "ruby", "py"]
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United State of America")
+map.set('Fr', "France")
+
+for (const key in map) {
+   console.log(key);
 }
