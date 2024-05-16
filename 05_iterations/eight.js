03beb0ff531f3e5b83eec02524040bc09b4d7fb1
Lokesh-Sah <120782223+Lokesh-Sah@users.noreply.github.com>
2024-05-16 13:44:59 +0000

    eight-iteration

---
+++ 05_iterations/eight.js
@@ -0,0 +1,38 @@
+// Reduce
+
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`)
+//     return acc + currval
+// }, 4)   // 4 comes first in acc
+
+const myTotal = myNums.reduce( (acc, currval) => acc + currval, 0)
+
+
+console.log(myTotal);
+
+
+
+//
+const shoppingCart = [
+    {
+        itemName : "js course",
+        price : 999
+    },
+    {
+        itemName : "java course",
+        price : 2999
+    },
+    {
+        itemName : "py course",
+        price : 1999
+    },
+    {
+        itemName : "mobile dev course",
+        price : 5999
+    }
+]
+
+const costToPay = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
+console.log(costToPay);
